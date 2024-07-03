d57e4e8876b41b6916a6663979a8c1e6c9d11906
Simon Walter <simon@walter-wm.de>
2024-07-03 22:28:08 +0200

    feat(communities_view): show loading spinner and welcome user when no community existing

--- lib/src/features/communities/views/communities_view.dart
+++ lib/src/features/communities/views/communities_view.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:activator_app/src/core/provider/db_provider.dart';
+import 'package:activator_app/src/core/utils/constants.dart';
+import 'package:activator_app/src/core/widgets/custom_progress_indicator.dart';
 import 'package:activator_app/src/features/communities/views/new_community_modal.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -71,85 +73,119 @@ class _CommunitiesViewState extends State<CommunitiesView> {
         child: Consumer<DatabaseProvider>(
           builder:
               (BuildContext context, DatabaseProvider value, Widget? child) {
-            return ListView.builder(
-              // Providing a restorationId allows the ListView to restore the
-              // scroll position when a user leaves and returns to the app after it
-              // has been killed while running in the background.
-              restorationId: 'CommunityListView',
-              itemCount: value.communities.length,
-              itemBuilder: (BuildContext context, int index) {
-                final item = value.communities[index];
+            if (!value.isInitialized) {
+              return const CustomProgressIndicator();
+            } else if (value.communities.isEmpty && value.isInitialized) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Image(
+                      image: AssetImage(
+                        'assets/images/rocket.png',
+                      ),
+                      width: 150,
+                      height: 150,
+                    ),
+                    const SizedBox(height: AppConstants.separatorSpacing),
+                    const Text('Welcome to Activator!',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Create a community to get started',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Theme.of(context).colorScheme.onSurfaceVariant,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return ListView.builder(
+                // Providing a restorationId allows the ListView to restore the
+                // scroll position when a user leaves and returns to the app after it
+                // has been killed while running in the background.
+                restorationId: 'CommunityListView',
+                itemCount: value.communities.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = value.communities[index];
 
-                return InkWell(
-                  onTap: () {
-                    routeBuilder(context) => CommunityDetailsView(
-                          communityId: item.id,
-                        );
+                  return InkWell(
+                    onTap: () {
+                      routeBuilder(context) => CommunityDetailsView(
+                            communityId: item.id,
+                          );
 
-                    Navigator.of(context).push(
-                      Platform.isIOS
-                          ? CupertinoPageRoute(
-                              builder: routeBuilder,
-                            )
-                          : MaterialPageRoute(
-                              builder: routeBuilder,
+                      Navigator.of(context).push(
+                        Platform.isIOS
+                            ? CupertinoPageRoute(
+                                builder: routeBuilder,
+                              )
+                            : MaterialPageRoute(
+                                builder: routeBuilder,
+                              ),
+                      );
+                    },
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: Icon(IconData(item.iconCode,
+                                fontFamily: 'MaterialIcons')),
+                          ),
+                          title: Text(item.name),
+                          subtitle: Text(item.description),
+                          trailing: SizedBox(
+                            width: 100,
+                            child: Stack(
+                              children: [
+                                // last session date in relation to the current date using formatDate function
+                                Align(
+                                  alignment: Alignment.topRight,
+                                  child: Text(
+                                    // formatDate(item.sessions!.last.endTime),
+                                    'last session date',
+                                    style: TextStyle(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSurfaceVariant,
+                                    ),
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                    overflow: TextOverflow.ellipsis,
+                                    // '${item.sessions?.length} session${item.sessions?.length == 1 ? '' : 's'}',
+                                    'session count',
+                                    style: TextStyle(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSurfaceVariant,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                    );
-                  },
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Icon(IconData(item.iconCode,
-                              fontFamily: 'MaterialIcons')),
-                        ),
-                        title: Text(item.name),
-                        subtitle: Text(item.description),
-                        trailing: SizedBox(
-                          width: 100,
-                          child: Stack(
-                            children: [
-                              // last session date in relation to the current date using formatDate function
-                              Align(
-                                alignment: Alignment.topRight,
-                                child: Text(
-                                  // formatDate(item.sessions!.last.endTime),
-                                  'last session date',
-                                  style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onSurfaceVariant,
-                                  ),
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.centerRight,
-                                child: Text(
-                                  overflow: TextOverflow.ellipsis,
-                                  // '${item.sessions?.length} session${item.sessions?.length == 1 ? '' : 's'}',
-                                  'session count',
-                                  style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onSurfaceVariant,
-                                  ),
-                                ),
-                              ),
-                            ],
                           ),
                         ),
-                      ),
-                      Divider(
-                        color: Theme.of(context).dividerColor.withOpacity(0.4),
-                        thickness: 0.5,
-                        indent: 72,
-                        height: 0,
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
+                        Divider(
+                          color:
+                              Theme.of(context).dividerColor.withOpacity(0.4),
+                          thickness: 0.5,
+                          indent: 72,
+                          height: 0,
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            }
           },
         ),
       ),
