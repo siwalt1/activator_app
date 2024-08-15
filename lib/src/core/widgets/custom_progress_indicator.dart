83e4a9c1ea70a32959d057dd6fb1ed1ede13e7f5
Simon Walter <simon@walter-wm.de>
2024-08-15 16:51:04 +0200

    Refactor CustomProgressIndicator to show loading spinner only after one second of loading

--- lib/src/core/widgets/custom_progress_indicator.dart
+++ lib/src/core/widgets/custom_progress_indicator.dart
@@ -5,13 +5,26 @@ class CustomProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      color: Theme.of(context).colorScheme.surface.withOpacity(0.5),
-      child: const Center(
-        child: CircularProgressIndicator(),
-      ),
+    return FutureBuilder(
+      future: Future.delayed(const Duration(seconds: 1)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Theme.of(context).colorScheme.surface.withOpacity(0.5),
+          );
+        } else {
+          return Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Theme.of(context).colorScheme.surface.withOpacity(0.5),
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
